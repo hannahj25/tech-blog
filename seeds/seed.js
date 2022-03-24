ccdefb6cd2d3ac9bdc651bddf7733bcfff81fc1b
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-24 18:11:45 +1030

    edited homepage.handlebars to include blog information

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/connection');
 const { User, Blog } = require('../models');
 
 const userData = require('./userData.json');
-const BlogData = require('./blogData.json');
+const blogData = require('./blogData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -11,6 +11,13 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
 
   process.exit(0);
