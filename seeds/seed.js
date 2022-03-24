b38390e8874b1e300ef2645696eb5aaeff0db503
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-25 01:42:04 +1030

    added comment data to seeds folder

--- seeds/seed.js
+++ seeds/seed.js
@@ -19,6 +19,12 @@ const seedDatabase = async () => {
     });
   }
 
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
 
   process.exit(0);
 };
