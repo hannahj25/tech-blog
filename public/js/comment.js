f64e41eb6defe176cedd003239a88597c56e65aa
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-25 12:58:24 +1030

    debugged comment function in comment.js, blog.handlebars

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,28 +1,32 @@
 const postButton = document.querySelector('#post-comment');
 
+console.log(postButton);
+
 postComment = async (event) => {
     event.preventDefault();
   
-    const comment = document.querySelector('#comment').value.trim();
-    
+    const comment = document.querySelector('.comment').value.trim();
+    const blog_id = window.location.pathname.slice(1).split("/")[1];
+    console.log({comment}, window.location.pathname);
+
   
     if (comment) {
       const response = await fetch(`/api/comments`, {
         method: 'POST',
-        body: JSON.stringify({ content }),
+        body: JSON.stringify({ content:comment, blog_id }),
         headers: {
           'Content-Type': 'application/json',
         },
       });
   
       if (response.ok) {
-        document.location.replace('/blog/:id');
+        document.location.replace(`/blog/${blog_id}`);
       } else {
         alert('Failed to post comment.');
       }
     }
   };
 
-  postButton.addEventListener("click", postComment())
+  postButton.addEventListener("click", postComment)
 
   
