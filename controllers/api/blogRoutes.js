ee6575106a1bfe6a373d4548a8aeb9461f52ee27
Hannah <94648556+hannahj25@users.noreply.github.com>
2022-03-25 12:10:22 +1030

    added home link to navbar, added put route to blogRoutes.js for editing blog posts

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -35,5 +35,24 @@ router.post('/', withAuth, async (req, res) => {
     }
   });
 
+  router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const blogData = await Blog.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (!blogData) {
+        res.status(404).json({message: 'No blog found with that id!'});
+        return;
+      }
+  
+      res.status(200).json(blogData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
 
